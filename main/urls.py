8abe533d7d440fa1d2b74c856a1cbaf3cac826b2
Andy Osborne <andyjamesosborne@proton.me>
2024-02-28 12:42:15 +0000

    adding static file root

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('profile', views.profile, name='profile'),
+    path('profile/', views.profile, name='profile'),
 ]
