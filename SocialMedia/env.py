bdd92cf7a1cc2a1fdf5be98e3027e85dfe9cb886
Andy Osborne <andyjamesosborne@proton.me>
2024-02-28 10:58:58 +0000

    Intital setup

---
+++ SocialMedia/env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgres://areboukz:i2snT7NMzUE2ib_HE-H5ZvSHgzUxuPwK@flora.db.elephantsql.com/areboukz")
+
+os.environ.setdefault(
+    "SECRET_KEY", "Andy")
