02c29e12c5ecea6f2d8f4b64114d21665a556731
Andy Osborne <andyjamesosborne@proton.me>
2024-02-28 12:30:47 +0000

    Config the url to html page home and profile

--- main/views.py
+++ main/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/base.html')
+    return render(request, 'main/home.html')
+
+def profile(request):
+    return render(request, 'main/profile.html')
